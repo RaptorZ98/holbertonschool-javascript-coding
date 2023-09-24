b7e41733c230a10673a05073b93aadd955e7ff9f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-09-24 12:50:17 -0700

    added argv 2 insted of 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const req = require('fs');
-const filePath = process.argv[0];
+const filePath = process.argv[2];
 req.readFile(filePath, 'utf-8', function(error, response){
     if(err){
         console.log(error)
